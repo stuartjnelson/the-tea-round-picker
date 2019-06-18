28c03b4826740a5a7bd104c609f7b0ef80096ef5
Stuart Nelson <stuart@weareready.com>
2019-06-18 20:59:35 +0100

    Added some styles yo spinner list

--- src/components/tea-round/spinner.vue
+++ src/components/tea-round/spinner.vue
@@ -1,7 +1,7 @@
 <template lang='html'>
     <div class="band">
-        <ul v-if="users">
-            <li v-for="(user, index) in users" :key="index">
+        <ul class="users-list" v-if="users">
+            <li class="users-list__item" v-for="(user, index) in users" :key="index">
                 {{ user.name }}
 
                 <button class="users__remove-btn" @click="removeUser(user.id)">Remove</button>
@@ -29,11 +29,22 @@
 </script>
 
 <style lang='scss' scoped>
+    @keyframes fade-in {
+        0% { opacity: 0; }
+
+        100% { opacity: 1; }
+    }
+
+    .users-list { @include inline-list($half-spacing-unit); }
+
+    .users-list__item { animation: fade-in 1 forwards linear 0.2s; }
+
     .users__remove-btn {
         @include get-color(grey, dark);
         @include fsz(tiny);
         border: 2px solid get-color(grey, dark);
         line-height: 1;
+        margin-left: $half-spacing-unit;
         padding: $quarter-spacing-unit / 2;
         transition: 0.15s linear border-color, 0.15s linear color;
 
