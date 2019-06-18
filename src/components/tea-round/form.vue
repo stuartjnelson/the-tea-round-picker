9ddf075cf1078f41ec95a70c78311e1c1c2a5c0c
Stuart Nelson <stuart@weareready.com>
2019-06-18 17:26:50 +0100

    Created tea round componetn files

---
+++ src/components/tea-round/form.vue
@@ -0,0 +1,14 @@
+<template lang='html'>
+    <form class="form">
+        <label for="name" class="form__label">Tea Maker</label>
+        <input id="name" class="form__input" type="text">
+    </form>
+</template>
+
+<script>
+    export default {
+    };
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/components/tea-round/spinner.vue
@@ -0,0 +1,21 @@
+<template lang='html'>
+    <div class="band">
+        <p v-for="(user, index) in users" :key="index">
+            {{ user.name }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            users: {
+                required: true,
+                type: Array
+            }
+        }
+    };
+</script>
+
+<style lang='scss' scoped>
+</style>
